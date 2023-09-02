79eadd45ed44518fdacf3ba578ce13c23a4c9702
YasuhiroNagahama <fnifhubi85h29bddi@gmail.com>
2023-09-02 20:37:49 +0900

    add controller method

--- controller.js
+++ controller.js
@@ -3,8 +3,17 @@
         function Controller() {
         }
         Controller.prototype.updateGameMode = function () {
-            var gameMode = document.getElementById("modeSelect");
+            var modeSelect = (document.getElementById("modeSelect"));
             var people = document.getElementById("peopleSelect");
+            modeSelect.addEventListener("click", function () {
+                var currentMode = modeSelect.value;
+                if (currentMode === "ai") {
+                    people.style.display = "none";
+                }
+                else if (currentMode === "player") {
+                    people.style.display = "flex";
+                }
+            });
         };
         return Controller;
     }());
--- controller.ts
+++ controller.ts
@@ -1,11 +1,39 @@
 {
   class Controller {
     public updateGameMode(): void {
-      const gameMode = document.getElementById("modeSelect");
-      const people = document.getElementById("peopleSelect");
+      const modeSelect = <HTMLSelectElement>(
+        document.getElementById("modeSelect")
+      );
+      const people = <HTMLInputElement>document.getElementById("peopleSelect");
+
+      modeSelect.addEventListener("click", () => {
+        const currentMode: string = modeSelect.value;
+
+        if (currentMode === "ai") {
+          people.style.display = "none";
+        } else if(currentMode === "player") {
+          people.style.display = "flex";
+        }
+      });
+    }
+
+    public startBlackjack(): void {
+
+    }
+
+    public startGame(): void {
+      const modeSelect = <HTMLSelectElement>document.getElementById("modeSelect");
+      const typeSelect = <HTMLSelectElement>document.getElementById("typeSelect");
+
+      const gameMode:string = modeSelect.value;
+      const gameType: string = typeSelect.value;
     }
   }
 
-  const controller = new Controller();
+  class BlackjackController extends Controller {
+
+  }
+
+  const controller: Controller = new Controller();
   controller.updateGameMode();
 }
